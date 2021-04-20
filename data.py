d5d9c8685338ac0d98250bf4cb5bdb4a4ae8c646
Shivansh Singhal <shivanshsinghal107@gmail.com>
2021-04-20 12:00:28 +0530

    Updated data.py for using sqlalchemy database

--- data.py
+++ data.py
@@ -1,7 +1,12 @@
-import sqlite3
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
 
-conn = sqlite3.connect("test.db")
+# Set up database
+engine = create_engine(os.getenv("DATABASE_URL", "sqlite:///database.db"))
+db = scoped_session(sessionmaker(bind=engine))
 
-conn.execute('''CREATE TABLE IF NOT EXISTS products (id INTEGER PRIMARY KEY AUTOINCREMENT, user_id INT NOT NULL, url TEXT NOT NULL, product_id VARCHAR(20) NOT NULL, title TEXT NOT NULL, tracking_price REAL, availability BIT NOT NULL, username VARCHAR(50), first_name VARCHAR(40), last_name VARCHAR(40))''')
-conn.commit()
-conn.close()
+db.execute('''CREATE TABLE IF NOT EXISTS products (id SERIAL PRIMARY KEY, user_id INT NOT NULL, url TEXT NOT NULL, product_id VARCHAR(20) NOT NULL, title TEXT NOT NULL, tracking_price REAL, availability BOOLEAN NOT NULL, username VARCHAR(50), first_name VARCHAR(40), last_name VARCHAR(40))''')
+
+db.commit()
+db.close()
