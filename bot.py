53883460c26002b81615e25ddbb8d1a2b0f8a6c5
Shivansh Singhal <shivanshsinghal107@gmail.com>
2021-04-20 17:38:23 +0530

    Changed port to 5000

--- bot.py
+++ bot.py
@@ -295,7 +295,7 @@ def main():
 	u.dispatcher.add_handler(CommandHandler("list", view_items))
 	u.dispatcher.add_handler(CommandHandler("remove", remove_all))
 	u.dispatcher.add_handler(CommandHandler("track", track))
-	u.start_webhook(listen="0.0.0.0", port=8443, url_path=TOKEN, webhook_url=f"https://trackamzn.herokuapp.com/{TOKEN}")
+	u.start_webhook(listen="0.0.0.0", port=5000, url_path=TOKEN, webhook_url=f"https://trackamzn.herokuapp.com/{TOKEN}")
 	u.idle()
     
 if __name__ == "__main__":
